f2f6332efd825728e7b8633245ac24532d5df4a8
Adam Aptowitz <64217755+Pugga12@users.noreply.github.com>
2026-06-02 15:03:19 -0400

    Remove fixed point math & gaussian noise
    We no longer use these, so remove them

--- include/dzsungel/constants.h
+++ include/dzsungel/constants.h
@@ -18,7 +18,6 @@ along with Dzsungel.  If not, see <http://www.gnu.org/license>
 */
 
 #pragma once
-#include "fix16.h"
 
 #ifndef M_PI
 #define M_PI (3.14159265358979323846264338327950288) // Define M_PI with high precision
@@ -32,12 +31,3 @@ along with Dzsungel.  If not, see <http://www.gnu.org/license>
 #ifndef TWO_PI_FLOAT
 #define TWO_PI_FLOAT (2 * M_PI)
 #endif
-
-#ifndef TWO_PI_FIX16
-#define TWO_PI_FIX16 (F16(TWO_PI_FLOAT))
-#endif
-
-#define FADD(a,b) fix16_add(a,b)
-#define FMUL(a,b) fix16_mul(a,b)
-#define FSUB(a,b) fix16_sub(a,b)
-#define FDIV(a,b) fix16_div(a,b)
